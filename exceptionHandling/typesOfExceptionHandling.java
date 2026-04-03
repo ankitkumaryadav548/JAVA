1d185a490e75cbd024114e2ed1c4b23a06395380
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-04-03 16:59:40 +0530

    update

---
+++ exceptionHandling/typesOfExceptionHandling.java
@@ -0,0 +1,26 @@
+// type of Exception habdling
+//1.try block 
+//2.catch
+//3.finally
+//4.throw
+//5.throws
+
+// try{
+
+// }
+
+// catch(ExpceptionType e)
+// {
+
+// }
+
+// finally{
+
+// }
+
+// throw => we will explicitily write any message => by thow 
+// throw new ArithmeticException("device by zero or second no. is zero");
+
+// throws => it will tell the method that which type of Exception we can get in code 
+
+// void show() throws IOexception , ClassNotFoundException , ArrayIndexOutBoundsException ;
