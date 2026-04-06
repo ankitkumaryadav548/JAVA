89a0a586767fabac60d48b6689f7054f4976e382
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-04-06 16:57:08 +0530

    update

---
+++ exceptionHandling/questionONcustomException.java
@@ -0,0 +1,27 @@
+//write code to create RuntimeException to check length of password
+//password should be String
+//password length should be greater than 10
+//name of passwordCheckException
+
+import java.util.*;
+
+class passwordCheckException extends RuntimeException{
+    passwordCheckException(String password){
+        super(password);
+    }
+}
+class Main{
+    public static void main(String[] args){
+        String password = "sjhvkjskvtg";
+        try{
+            if(password.length()>10){
+                throw new passwordCheckException("password is greater than 10");
+            }
+            }catch(passwordCheckException p){
+                System.out.println(p.getMessage());
+            }
+
+        
+
+    }
+}
