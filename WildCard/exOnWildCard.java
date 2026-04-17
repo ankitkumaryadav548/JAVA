523ad24feb524e285af12d04bbeef319be56b2a2
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-04-17 22:58:41 +0530

    update

---
+++ WildCard/exOnWildCard.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class WildcardExample {
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 3);
+        List<String> strList = Arrays.asList("A", "B", "C");
+
+        printList(intList);
+        printList(strList);
+    }
+}
