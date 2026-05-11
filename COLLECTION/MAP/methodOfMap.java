52bea4b278a135234520a613695bddd0574e776e
ankitkumaryadav548 <ankityadav02032003@gmail.com>
2026-05-11 18:40:49 +0530

    MAP

---
+++ COLLECTION/MAP/methodOfMap.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> map = new HashMap<>();
+        // put()
+        map.put(101, "Ankit");
+        map.put(102, "Rahul");
+        map.put(103, "Aman");
+
+        System.out.println("Original Map : "+ map);        
+        // get()
+        System.out.println("Value of key 101 : "+ map.get(101));
+        // containsKey()
+        System.out.println("Contains key 102 : "+ map.containsKey(102));
+        // containsValue()
+        System.out.println("Contains value Aman : "+ map.containsValue("Aman"));
+        // remove()
+        map.remove(102);
+
+        System.out.println("After remove : "+ map);
+        // size()
+        System.out.println("Size : " + map.size());               
+    }
+}
